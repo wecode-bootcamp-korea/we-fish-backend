1ed48643cdfacdbc5fe19776aab65acb0d1ea7ab
kay <k904808@gmail.com>
2020-02-25 22:15:11 +0900

    modify user/models and migrate

--- user/models.py
+++ user/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class User(models.Model):
-    email            = models.CharField(max_length = 300)
+    email            = models.CharField(max_length = 100)
     password         = models.CharField(max_length = 500)
     name             = models.CharField(max_length = 50)
-    date_of_birth    = models.CharField(max_length = 50)
-    postcode         = models.CharField(max_length = 50)
+    postcode         = models.CharField(max_length = 30)
     address          = models.CharField(max_length = 300)
-    detailed_address = models.CharField(max_length = 300)
-    mobile           = models.CharField(max_length = 100)
+    detailed_address = models.CharField(max_length = 300, null = True)
+    mobile           = models.CharField(max_length = 50)
+    agreement        = models.BooleanField(null = True)     #이용약관 동의여부
     created_at       = models.DateTimeField(auto_now_add = True)
     updated_at       = models.DateTimeField(auto_now = True)
 
