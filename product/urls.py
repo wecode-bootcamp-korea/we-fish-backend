9a1ae4a2dcfe598ce1bce292451cc415f28cfadf
whybein <swhybeing@gmail.com>
2020-02-28 18:27:46 +0900

    create product/view category

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views      import ProductListView, ProductView, ThemeView, ReviewView
+from .views      import CategoryView, ProductListView, ProductView, ThemeView, ReviewView
 
 urlpatterns = [
+    path('/category_list', CategoryView.as_view()),
     path('/product_list', ProductListView.as_view()),
     path('/product_info', ProductView.as_view()),
     path('/theme', ThemeView.as_view()),
