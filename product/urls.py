764522e63473eac53d5aab6b0cd37c89597c44d0
kay <k904808@gmail.com>
2020-02-29 22:31:52 +0900

    Create views by order in product/vies.py

--- product/urls.py
+++ product/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from .views import RateView, NewView, HotView, AscendingPriceView, DescendingPriceView
 
 urlpatterns = [
+    path('/rate', RateView.as_view()),
+    path('/new', NewView.as_view()),
+    path('/hot', HotView.as_view()),
+    path('/ascending-price', AscendingPriceView.as_view()),
+    path('/descending-price', DescendingPriceView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,164 @@
-from django.shortcuts import render
+from .models      import Category, Product, ProductCategory
 
-# Create your views here.
+from django.views import View
+from django.http  import HttpResponse, JsonResponse
+
+#추천순
+class RateView(View):
+    def get(self, request):
+        product_data = Product.objects.order_by('-rate').values('category__name','id','image_url','name','price')
+
+        product_list  = []
+        category_list = []
+        for product in product_data:
+            product_list.append(
+                {
+                    'category'  : product['category__name'],
+                    'id'        : product['id'],
+                    'image'     : product['image_url'],
+                    'name'      : product['name'],
+                    'price'     : product['price']
+                }
+            )
+
+        categories = [category for category in Category.objects.values()]
+        for i in range(len(categories)):
+            category_list.append(
+                {
+                    categories[i]['name']:
+                              [
+                                  item for item in product_list
+                               if item['category'] == categories[i]['name']
+                              ]
+                }
+            )
+
+        return JsonResponse({"rate":category_list}, status = 200)
+
+#신상품순
+class NewView(View):
+    def get(self, request):
+        product_data = Product.objects.order_by('-updated_at').values('category__name','id','image_url','name','price')
+
+        product_list  = []
+        category_list = []
+        for product in product_data:
+            product_list.append(
+                {
+                    'category'  : product['category__name'],
+                    'id'        : product['id'],
+                    'image'     : product['image_url'],
+                    'name'      : product['name'],
+                    'price'     : product['price']
+                }
+            )
+
+        categories = [category for category in Category.objects.values()]
+        for i in range(len(categories)):
+            category_list.append(
+                {
+                    categories[i]['name']:
+                              [
+                                  item for item in product_list
+                               if item['category'] == categories[i]['name']
+                              ]
+                }
+            )
+
+        return JsonResponse({"new":category_list}, status = 200)
+
+#인기상품순
+class HotView(View):
+    def get(self, request):
+        product_data = Product.objects.order_by('-sales').values('category__name','id','image_url','name','price')
+
+        product_list  = []
+        category_list = []
+        for product in product_data:
+            product_list.append(
+                {
+                    'category'  : product['category__name'],
+                    'id'        : product['id'],
+                    'image'     : product['image_url'],
+                    'name'      : product['name'],
+                    'price'     : product['price']
+                }
+            )
+
+        categories = [category for category in Category.objects.values()]
+        for i in range(len(categories)):
+            category_list.append(
+                {
+                    categories[i]['name']:
+                              [
+                                  item for item in product_list
+                               if item['category'] == categories[i]['name']
+                              ]
+                }
+            )
+
+        return JsonResponse({"hot":category_list}, status = 200)
+
+#낮은 가격순
+class AscendingPriceView(View):
+    def get(self, request):
+        product_data = Product.objects.order_by('price').values('category__name','id','image_url','name','price')
+
+        product_list  = []
+        category_list = []
+        for product in product_data:
+            product_list.append(
+                {
+                    'category'  : product['category__name'],
+                    'id'        : product['id'],
+                    'image'     : product['image_url'],
+                    'name'      : product['name'],
+                    'price'     : product['price']
+                }
+            )
+
+        categories = [category for category in Category.objects.values()]
+        for i in range(len(categories)):
+            category_list.append(
+                {
+                    categories[i]['name']:
+                              [
+                                  item for item in product_list
+                               if item['category'] == categories[i]['name']
+                              ]
+                }
+            )
+
+        return JsonResponse({"ascending_price":category_list}, status = 200)
+
+#높은 가격순
+class DescendingPriceView(View):
+    def get(self, request):
+        product_data = Product.objects.order_by('-price').values('category__name','id','image_url','name','price')
+
+        product_list  = []
+        category_list = []
+        for product in product_data:
+            product_list.append(
+                {
+                    'category'  : product['category__name'],
+                    'id'        : product['id'],
+                    'image'     : product['image_url'],
+                    'name'      : product['name'],
+                    'price'     : product['price']
+                }
+            )
+
+        categories = [category for category in Category.objects.values()]
+        for i in range(len(categories)):
+            category_list.append(
+                {
+                    categories[i]['name']:
+                              [
+                                  item for item in product_list
+                               if item['category'] == categories[i]['name']
+                              ]
+                }
+            )
+
+        return JsonResponse({"descending_price":category_list}, status = 200)
