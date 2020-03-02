d0dc0843fde008257801848b371ac90b10475907
whybein <swhybeing@gmail.com>
2020-03-02 22:58:15 +0900

    fix product SearchView

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views      import CategoryView, ThemeView, ReviewView
+from .views      import CategoryView, ThemeView, SearchView
 
 urlpatterns = [
     path('/category_list', CategoryView.as_view()),
     path('/theme', ThemeView.as_view()),
-    path('/reviews', ReviewView.as_view()),
+    path('/reviews', SearchView.as_view()),
 ]
