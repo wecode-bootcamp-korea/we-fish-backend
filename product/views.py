58346e13528a100cd774c055bee996a89b39ef44
whybein <swhybeing@gmail.com>
2020-03-03 20:23:45 +0900

    fix search view id and add feature

--- product/views.py
+++ product/views.py
@@ -47,8 +47,8 @@ class SearchView(View):
     def get(self, request):
         search_word = request.GET.get('keyword', '')
         try:
-            search_data = Product.objects.filter(name__icontains=search_word).values('name', 'price', 'image_url')
-            if not search_data:
+            search_data = Product.objects.filter(name__icontains=search_word).values('id', 'name', 'price', 'image_url')
+            if not search_data or search_word == '':
                 return JsonResponse({"message":"No Results."}, status = 200)
             return JsonResponse({"search_results":list(search_data)}, status = 200)
 
