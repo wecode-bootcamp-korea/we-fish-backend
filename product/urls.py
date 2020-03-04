f1bd82c8a62f991ea39e923f9a1ed546828cda53
whybein <swhybeing@gmail.com>
2020-03-04 21:34:09 +0900

    fix prodcut/urls comma

--- product/urls.py
+++ product/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('/category_list', CategoryView.as_view()),
     path('/theme', ThemeView.as_view()),
     path('/detail/<int:product_id>', DetailView.as_view()),
-    path('', ProductListView.as_view())
+    path('', ProductListView.as_view()),
     path('/search', SearchView.as_view()),
 ]
