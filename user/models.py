6d1dcad0fe469c12ce692d3b4844affe5867c9d4
kay <k904808@gmail.com>
2020-03-05 18:31:03 +0900

    Alter models.py and views.py (verification)

--- user/models.py
+++ user/models.py
@@ -19,7 +19,7 @@ class User(models.Model):
 class Verification(models.Model):
     mobile     = models.CharField(max_length = 50, null = True)
     code       = models.CharField(max_length = 20, null = True)
-    count      = models.CharField(max_length = 20, null = True)
+    count      = models.IntegerField(null = True)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
